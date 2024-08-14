07b2d3886f2fd988d0ae1362b629176f7de6c64e
karthicbabuk-kg <karthicbabu333@gmail.com>
2024-08-14 12:56:02 +0530

    new

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const app = express();
 
 router.get('/authenticate', authController.authenticateUser);
+app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/admin/*', authController.ensureAuthenticated, (req, res) => {
     if (req.session.user.role === 'admin') {
