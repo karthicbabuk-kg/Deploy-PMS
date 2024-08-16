96f9165ecaff382857d57c42cb444a70c477bcc3
karthicbabuk-kg <karthicbabu333@gmail.com>
2024-08-16 14:36:45 +0530

    new

--- controllers/extaskController.js
+++ controllers/extaskController.js
@@ -16,7 +16,7 @@ exports.addTask = async (req, res) => {
 
     try {
         await taskModel.addTask(taskData);
-        res.redirect('../executive/viewtask.html'); // Redirect after successful insertion
+        res.redirect('../executive/uploastask.html'); // Redirect after successful insertion
     } catch (error) {
         console.error('Database insert error:', error);
         res.status(500).send('Server error');
